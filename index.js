bcf3d9e9e11f82cd5a65892f87335f15e0a645a1
Adam Miner <miner.adam1@hotmail.com>
2017-06-27 14:49:11 -0600

    Here

--- index.js
+++ index.js
@@ -13,13 +13,5 @@ $(document).ready(function(){
     } else {
       console.log(return $.post('https://littlehelpers.herokuapp.com/auth/register', signUp))
     }
-    function createUser(id) {
-      return $.post({
-        url: `${API_URL}/user/${id}`,
-        headers: {
-          Authorization: `Bearer ${localStorage.token}`
-        }
-      });
-    };
   })
 })
