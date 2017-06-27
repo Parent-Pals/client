47570886008196bc45594bd2d3d0ccfe490a4894
Chase Hemerda <chasehemerda@icloud.com>
2017-06-27 15:42:30 -0600

    change

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var apiUrl = "https://littlehelpers.herokuapp.com/parent/1";
+  var apiUrl = `https://littlehelpers.herokuapp.com/parent/${localStorage.id}`;
   $.getJSON(apiUrl).then(function(data){
     var parentSource = $("#parent-template").html();
     var parentTemplate = Handlebars.compile(parentSource);
