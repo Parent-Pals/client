12e9d8b0df3b3ee65309b6697be46350dfc1bef0
Adam Miner <miner.adam1@hotmail.com>
2017-06-28 11:24:36 -0600

    Formatting

--- child.js
+++ child.js
@@ -3,53 +3,70 @@ $(document).ready(function(){
   var apiUrl = "https://littlehelpers.herokuapp.com/parent/1/5";
 
   $.getJSON(apiUrl).then(function(data){
+
     console.log(data);
 
     for(var i = 0; i < data[1].length; i++){
+
       var rewardSource = $("#reward-template").html();
       var rewardTemplate = Handlebars.compile(rewardSource);
       var rewardContext = {
         "rewardName": data[1][i].name,
         "rewardPoints": data[1][i].point_value
       };
-      $("#childRewards").prepend(rewardTemplate(rewardContext))
+
+      $("#childRewards").prepend(rewardTemplate(rewardContext));
+
     };
 
     for(var i=0; i < data[2].length; i++){
+
       var taslSource = $("#task-template").html();
       var taskTemplate = Handlebars.compile(taskSource);
       var taskContext = {
         "taskName": data[2][i].name,
         "taskPoints": data[2][i].point_value
       };
+
       $("#childTasks").prepend(taskTemplate(taskContext))
+
     };
 
     for (var i = 0; i < data[0].length; i++) {
+
       var childSource = $("#child-template").html();
       var childTemplate = Handlebars.compile(childSource);
       var childContext = {
         "childName": data[0][i].name,
         "childPoints": data[0][i].points
       };
+
     };
 
     $('#childName').append(childTemplate(childContext));
+
   });
 
   $("#addReward").click(function(){
+
     var rewardObject = {
       name: $("rewardName").val(),
       point_value: $("rewardPointWorth").val()
     };
+
     $.post('https://littlehelpers.herokuapp.com/parent/{id}/{childID}/reward', rewardObject);
+
   });
 
   $("#addTask").click(function(){
+
     var rewardObject = {
       name: $("taskName").val(),
       point_value: $("chorePointWorth").val()
     };
+
     $.post('https://littlehelpers.herokuapp.com/parent/{id}/{childID}/task', rewardObject);
+
   });
+  
 });
--- dashboard.js
+++ dashboard.js
@@ -3,44 +3,65 @@ $(document).ready(function() {
   const API_URL = `https://littlehelpers.herokuapp.com/parent/`;
 
   $.ajax({
+
     url: `${API_URL}${localStorage.id}`,
     headers:{'Authorization': `Bearer ${localStorage.token}`},
     type: 'GET'
+
   })
+
   .then(function(data) {
-    console.log(data)
+    
+    console.log(data);
+
     var parentSource = $("#parent-template").html();
     var parentTemplate = Handlebars.compile(parentSource);
     var parentContext = {
       "parentName": data[0][0].name
     };
-    $("#navbarSupportedContent").prepend(parentTemplate(parentContext))
+
+    $("#navbarSupportedContent").prepend(parentTemplate(parentContext));
+
     var childSource   = $("#child-template").html();
     var childTemplate = Handlebars.compile(childSource);
+
     for(var i=0; i < data[1].length; i++){
       var childContext = {
         "childName": data[1][i].name,
         "childPoints": data[1][i].points
       };
+
       $("#children").prepend(childTemplate(childContext));
-    }
+
+    };
+
   });
 
   $("#addChild").click(function(){
+
     var childName = $("#childNameAdd").val();
+
     return $.ajax({
+
       url: `${API_URL}2`,
       headers:{'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNDk4NTk0NzMxLCJleHAiOjE0OTg2MDE5MzF9.4lIV67OwsP8f9btLNoDwFxah_TSkpsExcJoEUsfI7O4`},
       type: 'POST',
       data: {childName}
+
     });
+
   });
 
   $("#deleteChild").click(function(){
-    return $.ajax({
+
+    return $.get({
+
       url: `${API_URL}2/1`,
       headers:{'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNDk4NTk0NzMxLCJleHAiOjE0OTg2MDE5MzF9.4lIV67OwsP8f9btLNoDwFxah_TSkpsExcJoEUsfI7O4`},
       type: 'DELETE'
+
     });
+
   });
+
 });
