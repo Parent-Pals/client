53c96e25579174a67d65fdd0b41c20a599f220ad
Adam Miner <miner.adam1@hotmail.com>
2017-06-28 15:47:34 -0600

    Add reward and task

--- child.js
+++ child.js
@@ -44,17 +44,22 @@ $(document).ready(function(){
     $('#childName').append(childTemplate(childContext));
   });
 
-  $("#addReward").click(function(){
+  $("#addReward").click(function(event) {
+    event.preventDefault();
     var rewardObject = {
-      name: $("rewardName").val(),
-      point_value: $("rewardPointWorth").val()
+      name: $("#rewardName").val(),
+      point_value: $("#rewardPointWorth").val()
     };
-    $.ajax({
-      url: `${API_URL}${localStorage.id}/${child_id}/reward`,
+    return $.ajax({
+      url: `${API_URL}reward/${localStorage.id}/${child_id}`,
       headers:{'Authorization': `Bearer ${localStorage.token}`},
       type: 'POST',
       body: rewardObject
     })
+      .then(function(result) {
+        console.log(result);
+        window.location.reload();
+      })
   })
 
   function logOut(){
@@ -67,16 +72,21 @@ $(document).ready(function(){
     }
     logOut()
 
-  $("#addTask").click(function(){
+  $("#addTask").click(function(event) {
+    event.preventDefault();
     var taskObject = {
-      name: $("taskName").val(),
-      point_value: $("chorePointWorth").val()
+      name: $("#taskName").val(),
+      point_value: $("#chorePointWorth").val()
     };
-    $.ajax({
-      url: `${API_URL}${localStorage.id}/${child_id}/task`,
+    return $.ajax({
+      url: `${API_URL}task/${localStorage.id}/${child_id}/`,
       headers:{'Authorization': `Bearer ${localStorage.token}`},
       type: 'POST',
       body: taskObject
+    })
+      .then(function(result) {
+        console.log(result);
+        window.location.reload();
     })
   });
 });
