10b996ea66df85fb991520723136f59bc589cca6
Matthew Gonzer <matthewgonzer@gmail.com>
2017-06-28 08:27:42 -0600

    add login

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-// $(document).ready(function(){
-//   $("#createAccountButton").click(function(){
-//     event.preventDefault()
-//
-//     var signUp = {
-//       password: $("#password").val(),
-//       passwordConfirmation: $("#passwordConfirmation").val(),
-//       name: $("#name").val(),
-//       email: $("#email").val()
-//     }
-//     console.log(signUp);
-//     if(signUp.password != signUp.passwordConfirmation){
-//       $(".passwordError").css('display', 'block')
-//     } else {
-//       return $.post('https://littlehelpers.herokuapp.com/auth/register', signUp)
-//     }
-//   })
-// })
-//
 $(document).ready(function(){
   $("#createAccountButton").click(function(){
     event.preventDefault()
@@ -37,30 +18,28 @@ $(document).ready(function(){
         localStorage.id = result.id;
         window.location = `/dashboard.html`
 
->>>>>>> origin/master
-
       }).catch(error =>{
         console.error(error)
           showErrorMessage(error.responseJSON.message)
       })
     }
+  });
+
+  $("#loginButton").click(function(){
+    event.preventDefault()
+    let parent = {
+      email: $("#loginEmail").val(),
+      passoword: $("loginPassword").val()
+    }
+    $.post('https://littlehelpers.herokuapp.com/auth/login', parent)
+    .then(result => {
+      console.log(result);
+      localStorage.token = result.token;
+      localStorage.id = result.id;
+      window.location = `/dashboard.html`
+    }).catch(error => {
+      console.error(error)
+        showErrorMessage(error.responseJSON.message)
+    })
   })
 })
-
-
-//
-// $(()=>{
-//   $('form').submit((event)=>{
-//     event.preventDefault()
-//     const user = getUserFromForm();
-//     signup(user)
-//       .then(result=>{
-//         console.log(result);
-//         localStorage.token = result.token;
-//         setIdRedirect(result);
-//       }).catch(error =>{
-//         console.error(error)
-//           showErrorMessage(error.responseJSON.message)
-//       })
-//   });
-// })
