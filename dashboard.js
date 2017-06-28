1b17a629b3aa6e08d4f1071703952e8ad9850a88
Marlena Baker <baker.marlena@gmail.com>
2017-06-28 10:15:00 -0600

    child list add dynamic links

--- dashboard.js
+++ dashboard.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   API_URL = "https://littlehelpers.herokuapp.com/parent/";
   $.ajax({
-    url: `${API_URL}2`,
-    headers:{'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNDk4NTk0NzMxLCJleHAiOjE0OTg2MDE5MzF9.4lIV67OwsP8f9btLNoDwFxah_TSkpsExcJoEUsfI7O4`},
+    url: `${API_URL}${localStorage.id}`,
+    headers:{'Authorization': `Bearer ${localStorage.token}`},
     type: 'GET'
   })
   .then(function(data) {
@@ -18,7 +18,8 @@ $(document).ready(function() {
     for(var i=0; i < data.length; i++){
         var childContext = {
           "childName": data[1][i].name,
-          "childPoints": data[1][i].points
+          "childPoints": data[1][i].points,
+          "childURL": `https://little-helpers-b26e7.firebaseapp.com/child.html/?parent_id=${localStorage.id}&child_id=${data[1][i].id}`
         }
           $("#children").append(childTemplate(childContext))
     }
@@ -26,16 +27,16 @@ $(document).ready(function() {
   $("#addChild").click(function(){
     var childName = $("#childNameAdd").val();
     return   $.ajax({
-        url: `${API_URL}2`,
-        headers:{'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNDk4NTk0NzMxLCJleHAiOjE0OTg2MDE5MzF9.4lIV67OwsP8f9btLNoDwFxah_TSkpsExcJoEUsfI7O4`},
+        url: `${API_URL}${localStorage.id}`,
+        headers:{'Authorization': `Bearer ${localStorage.token}`},
         type: 'POST',
         data: {childName}
       })
   });
   $("#deleteChild").click(function(){
     return   $.ajax({
-        url: `${API_URL}2/1`,
-        headers:{'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNDk4NTk0NzMxLCJleHAiOjE0OTg2MDE5MzF9.4lIV67OwsP8f9btLNoDwFxah_TSkpsExcJoEUsfI7O4`},
+        url: `${API_URL}${localStorage.id}/`,
+        headers:{'Authorization': `Bearer ${localStorage.token}`},
         type: 'DELETE'
       })
     });
