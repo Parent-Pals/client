ce5d26559afdd9f0c60be16204e80779d754d06e
Chase Hemerda <chasehemerda@icloud.com>
2017-06-29 14:51:21 -0600

    admin page

--- admin.js
+++ admin.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
     var API_URL = "https://littlehelpers.herokuapp.com/admin/1";
+
     $.ajax({
     url: `${API_URL}`,
     headers:{'Authorization': `Bearer ${localStorage.token}`},
     type: 'GET'
-  })
-
+  }).then(function(data){
+    console.log(data)
+    for(var i=0; i<data.length; i++){
+      var adminSource = $("#admin-template").html();
+      var adminTemplate = Handlebars.compile(adminSource);
+      var userNames = {
+        "user": data[i].name
+      }
+       $('#users').append(adminTemplate(userNames));
+    }
+  });
 
   function logOut(){
       $("#logout").click(function(){
